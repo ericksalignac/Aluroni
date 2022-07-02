1c523382a2b2f4f029c9d32184e95b911379811b
ericksalignac <ericksalignac@gmail.com>
2022-07-02 16:18:26 -0300

    Reaproveitando o Header

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,5 +1,6 @@
 import styles from './Menu.module.scss';
 import { ReactComponent as Logo } from 'assets/logo.svg'
+import { Link } from 'react-router-dom';
 
 export default function Menu() {
 const rotas = [{
@@ -16,9 +17,9 @@ const rotas = [{
       <ul className={styles.menu__list}>
         {rotas.map((rota, index) => (
           <li key={index} className={styles.menu__link}>
-            <a href={rota.to}>
+            <Link to={rota.to}>
               {rota.label}
-            </a>
+            </Link>
           </li>
         ))}
       </ul>
