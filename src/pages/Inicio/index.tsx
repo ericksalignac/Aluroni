c7d08bd4fe184def42c6d4c861f46f58223ca7ac
ericksalignac <ericksalignac@gmail.com>
2022-07-02 13:16:16 -0300

    Reaproveitando o menu

--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -1,3 +1,4 @@
+import Menu from 'components/Menu';
 import cardapio from 'data/cardapio.json'
 import styles from './Inicio.module.scss';
 
@@ -7,6 +8,7 @@ pratosRecomendados = pratosRecomendados.sort(() => 0.5 - Math.random()).splice(0
 
   return (
     <section>
+      <Menu />
       <h3 className={styles.titulo}>
         Recomendações da cozinha
       </h3>
