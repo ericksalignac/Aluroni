93c34a4cd0b7308a1a85a7a4a8d07aa992155075
ericksalignac <ericksalignac@gmail.com>
2022-07-08 00:09:43 -0300

    Terminando a tela de sobre

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,9 @@ import Menu from 'components/Menu';
 import PaginaPadrao from 'components/PaginaPadrao';
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
+import Sobre from 'pages/Sobre';
 import { BrowserRouter as Router , Routes, Route } from 'react-router-dom';
+
 export default function AppRouter(){
   return (
     <main>
@@ -12,6 +14,7 @@ export default function AppRouter(){
           <Route path='/' element={<PaginaPadrao />}>
           <Route index element={<Inicio/>}/>  
           <Route path='cardapio' element={<Cardapio />}/>
+          <Route path='sobre' element={<Sobre />}/>
           </Route>
         </Routes>
       </Router>
