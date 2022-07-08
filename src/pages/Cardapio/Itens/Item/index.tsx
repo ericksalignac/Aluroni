acaa1f1391a40dc98b490928987c5818d46aa6de
ericksalignac <ericksalignac@gmail.com>
2022-07-08 15:27:51 -0300

    Refatorando tags

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,37 +1,22 @@
 import styles from './Item.module.scss'
 import classNames from 'classnames';
 import { Prato } from 'types/Prato';
+import TagsPrato from 'components/TagsPrato';
 
-export default function Item(props: Prato){
-    const {title, description, category, size, serving, price, photo} = props;
-    return(
-        <div className={styles.item}>
-            <div className={styles.item__imagem}>
-                <img src={photo} alt={title} />
-            </div>
-            <div className={styles.item__descricao}>
-                <div className={styles.item__titulo}>
-                    <h2>{title}</h2>
-                    <p>{description}</p>
-                </div>
-                <div className={styles.item__tags}>
-                    <div className={classNames({
-                        [styles.item__tipo]: true,
-                        [styles[`item__tipo__${category.label.toLowerCase()}`]] : true
-                    })}>
-                        {category.label}
-                    </div>
-                    <div className={styles.item__porcao}>
-                        {size}g
-                    </div>
-                    <div className={styles.item__qtdpessoas}>
-                        Serve {serving} pessoa{serving === 1 ? "" : "s"}
-                    </div>
-                    <div className={styles.item__valor}>
-                        R${price.toFixed(2)}
-                    </div>
-                </div>
-            </div>
+export default function Item(props: Prato) {
+  const { title, description, photo } = props;
+  return (
+    <div className={styles.item}>
+      <div className={styles.item__imagem}>
+        <img src={photo} alt={title} />
+      </div>
+      <div className={styles.item__descricao}>
+        <div className={styles.item__titulo}>
+          <h2>{title}</h2>
+          <p>{description}</p>
         </div>
-    );
+        <TagsPrato {...props} />
+      </div>
+    </div>
+  );
 }
